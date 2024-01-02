b36b976507d98e68b3d647b9745cbda27522fe36
Rajesh1308 <rajesharulrathna@gmail.com>
2024-01-02 20:02:05 +0530

    Animations added for cards and progress bars

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,15 @@ function hideSidebar() {
     var sidebar = document.querySelector(".sidebar");
     sidebar.style.display = 'none';
 }
+
+const observer = new IntersectionObserver((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+    
+},{
+    threshold: 0.5
+});
+
+const aboutElement = document.querySelector(".about")
+observer.observe(aboutElement)
